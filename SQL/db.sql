8d50ec92f351ca4941a18caa53c0ea02d8d0c9e0
leyan <leyan@DESKTOP-ROKIKRN>
2023-03-06 08:03:13 +0900

    m

--- SQL/db.sql
+++ SQL/db.sql
@@ -46,3 +46,16 @@ CREATE TABLE `t_area` (
   `dong` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`idx`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO `` (`u_id`,`u_password`,`u_name`,`u_nickname`,`u_tel`,`u_email`,`u_address`,`createDate`,`changeDate`) VALUES ('leele','1234','이레얀','테스트','01000000000','leele@lee.com','인천광역시 계양구 작전동','2023-03-05 21:28:16','2023-03-05 21:28:16');
+INSERT INTO `` (`u_id`,`u_password`,`u_name`,`u_nickname`,`u_tel`,`u_email`,`u_address`,`createDate`,`changeDate`) VALUES ('leele2','1234','eee','test23','01000000000','leele2@lee.com','경상북도 영덕군 영덕읍','2023-03-05 22:59:50','2023-03-05 22:59:50');
+INSERT INTO `` (`board_idx`,`img`,`thumbnail`,`u_id`) VALUES (223,'\\img\\292cb85e-5520-4904-82fd-1b5419916bce.avif','y','leele');
+INSERT INTO `` (`board_idx`,`img`,`thumbnail`,`u_id`) VALUES (223,'\\img\\3447f53b-ac62-4c46-9546-125bbcf1ae7b.avif','n','leele');
+INSERT INTO `` (`board_idx`,`img`,`thumbnail`,`u_id`) VALUES (225,'\\img\\614b8063-37c2-4332-8b7b-097cccc32b95.jpg','y','leele');
+INSERT INTO `` (`m_no`,`u_sender`,`u_recipient`,`m_title`,`m_content`,`createDate`,`u_id`) VALUES (113,'test23','테스트','hello','hi','2023-03-05 23:00:59','leele2');
+INSERT INTO `` (`board_idx`,`b_title`,`b_content`,`u_nickname`,`u_address`,`p_price`,`createDate`,`changeDate`,`u_id`) VALUES (223,'TEST01','TEST','테스트','서울특별시 종로구 청운동',5000,'2023-03-05 21:53:26','2023-03-05 21:53:26','leele');
+INSERT INTO `` (`board_idx`,`b_title`,`b_content`,`u_nickname`,`u_address`,`p_price`,`createDate`,`changeDate`,`u_id`) VALUES (225,'test2','123','테스트','부산광역시 중구 영주동',50000,'2023-03-05 21:59:21','2023-03-05 21:59:21','leele');
+
+
+
+
